b6be84b27dd05b093a077fde94bc8d4a82248e1a
Anton Kovalchuck <antonkovalchuck@gmail.com>
2021-12-12 20:36:44 +0200

    Update listContacts.js

--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -5,14 +5,12 @@ const contactsPath = path.resolve(__dirname, '../../db/contacts.json')
 
 const listContacts = async () => {
     try {
-        // const temp = await fs.readFile(contactsPath, "utf8")
-        // console.table(await fs.readFile(contactsPath, "utf8"))
         return JSON.parse(await fs.readFile(contactsPath, "utf8"))
     } catch (error) {
         console.log(error)
     }
 }
-// listContacts()
+
 module.exports = {
     listContacts,
     contactsPath,
